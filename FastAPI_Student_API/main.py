7b51451d258add09d054bf9d11701030a8df2d26
arneeraja4 <arneeraja4@gmail.com>
2026-06-29 19:31:51 +0530

    Added FastAPI CRUD with PostgreSQL

--- FastAPI_Student_API/main.py
+++ FastAPI_Student_API/main.py
@@ -1,33 +1,54 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+import crud
+from database import SessionLocal, engine
 
 app = FastAPI()
 
-class Student(BaseModel):
-    id: int
-    name: str
-    age: int
+models.Base.metadata.create_all(bind=engine)
 
-students = [
-    {"id": 1, "name": "Rahul", "age": 20},
-    {"id": 2, "name": "Anu", "age": 19}
-]
 
-@app.get("/students")
-def get_students():
-    return students
+# Database Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/students/{id}")
-def get_student(id: int):
-    for student in students:
-        if student["id"] == id:
-            return student
-    return {"message": "Student not found"}
 
-@app.post("/students")
-def add_student(student: Student):
-    students.append(student.model_dump())
-    return {
-        "message": "Student added successfully",
-        "student": student
-    }
+@app.post("/students", response_model=schemas.StudentResponse)
+def create_student(student: schemas.StudentCreate, db: Session = Depends(get_db)):
+    return crud.create_student(db, student)
+
+
+@app.get("/students", response_model=list[schemas.StudentResponse])
+def get_students(db: Session = Depends(get_db)):
+    return crud.get_students(db)
+
+
+@app.get("/students/{student_id}", response_model=schemas.StudentResponse)
+def get_student(student_id: int, db: Session = Depends(get_db)):
+    student = crud.get_student(db, student_id)
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+
+
+@app.put("/students/{student_id}", response_model=schemas.StudentResponse)
+def update_student(student_id: int, student: schemas.StudentUpdate, db: Session = Depends(get_db)):
+    updated_student = crud.update_student(db, student_id, student)
+    if not updated_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return updated_student
+
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int, db: Session = Depends(get_db)):
+    deleted_student = crud.delete_student(db, student_id)
+    if not deleted_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return {"message": "Student deleted successfully"}
